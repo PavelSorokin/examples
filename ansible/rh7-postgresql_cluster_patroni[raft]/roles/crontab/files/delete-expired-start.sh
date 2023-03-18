7c6cd1cfda963324c599825b3dbfe6964256de9e
Pavel Sorokin <p.sorokin@db-service.ru>
2023-03-18 20:12:07 +0300

    new init

--- ansible/rh7-postgresql_cluster_patroni[raft]/roles/crontab/files/delete-expired-start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [  -f /tmp/backup.lock ]; then
-        echo "Script is working already"
-        pidlock=`cat /tmp/backup.lock`
-        ps -ef | grep $pidlock
-        exit 1
-fi
-/home/postgres/scripts/delete-expired.sh $1 >> /home/postgres/log/$(date +%Y%m%d).log 2>&1
--- ansible/rh7-postgresql_cluster_patroni[raft]/roles/crontab/files/zbx_backup_check.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-instance=$1
-pg_probackup=$2
-if [ "$($pg_probackup show --instance=$instance -B /pg_backup --format=json | jq -c '.[].backups[0].status')" == '"OK"' ];
-        then   result=0;
-        else   result=1;
-fi
-/bin/zabbix_sender -c /etc/zabbix/zabbix_agent2.conf -k pgsql.db.backup -o $result
